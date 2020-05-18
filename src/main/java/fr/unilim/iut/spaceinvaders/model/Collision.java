4c6447f355fdba279c97b78d7a38e0537c14f431
Florian Vivet <florian.vivet@etu.unilim.fr>
2020-05-18 11:58:39 +0200

    Correction d'une erreur de frappe

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
@@ -9,32 +9,32 @@ public class Collision {
 		if (seSuperposeVerticalement(sprite1, sprite2)) {
 			
 			//Collision frontale
-			if(seSuperposeHorinzontalement(sprite1, sprite2)) { 
+			if(seSuperposeHorizontalement(sprite1, sprite2)) { 
 				return true;
 			}
 			
 			//Collision latérale gauche 
-			if(seSuperposeHorinzontalementPartiellementAGauche(sprite1, sprite2)) { 
+			if(seSuperposeHorizontalementPartiellementAGauche(sprite1, sprite2)) { 
 				return true;
 				}
 			
 			//Collision latérale droite
-			if(seSuperposeHorinzontalementPartiellementADroite(sprite1, sprite2)) { 
+			if(seSuperposeHorizontalementPartiellementADroite(sprite1, sprite2)) { 
 				return true;
 				}
 		}
 		return false;
 	}
 
-	private boolean seSuperposeHorinzontalementPartiellementADroite(Sprite sprite1, Sprite sprite2) {
+	private boolean seSuperposeHorizontalementPartiellementADroite(Sprite sprite1, Sprite sprite2) {
 		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite()-(sprite1.longueur()-1) <= sprite2.abscisseLaPlusADroite();
 	}
 
-	private boolean seSuperposeHorinzontalementPartiellementAGauche(Sprite sprite1, Sprite sprite2) {
+	private boolean seSuperposeHorizontalementPartiellementAGauche(Sprite sprite1, Sprite sprite2) {
 		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche()-(sprite1.longueur()-1) && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite();
 	}
 
-	private boolean seSuperposeHorinzontalement(Sprite sprite1, Sprite sprite2) {
+	private boolean seSuperposeHorizontalement(Sprite sprite1, Sprite sprite2) {
 		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite();
 	}
 
