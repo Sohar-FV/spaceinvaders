05a4f49c6bbf127031099f75ee0007b073429218
Florian Vivet <florian.vivet@etu.unilim.fr>
2020-05-18 11:57:45 +0200

    Refactoring Collisions

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Collision.java
@@ -5,29 +5,41 @@ public class Collision {
 	public boolean detecterCollision(Sprite sprite1, Sprite sprite2) {
 		
 		
-		 	
-		boolean bool = false;
-		
-		
 		//Condition de collision verticale (obligatoire dans tous les cas)
-		if (sprite1.ordonneeLaPlusBasse() <= sprite2.ordonneeLaPlusHaute() && sprite1.ordonneeLaPlusHaute() >= sprite2.ordonneeLaPlusBasse()) {
+		if (seSuperposeVerticalement(sprite1, sprite2)) {
 			
 			//Collision frontale
-			if(sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite()) { 
+			if(seSuperposeHorinzontalement(sprite1, sprite2)) { 
 				return true;
 			}
 			
 			//Collision latérale gauche 
-			if(sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche()-(sprite1.longueur()-1) && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite()) { 
+			if(seSuperposeHorinzontalementPartiellementAGauche(sprite1, sprite2)) { 
 				return true;
 				}
 			
 			//Collision latérale droite
-			if(sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite()-(sprite1.longueur()-1) <= sprite2.abscisseLaPlusADroite()) { 
+			if(seSuperposeHorinzontalementPartiellementADroite(sprite1, sprite2)) { 
 				return true;
 				}
 		}
-		return bool;
+		return false;
+	}
+
+	private boolean seSuperposeHorinzontalementPartiellementADroite(Sprite sprite1, Sprite sprite2) {
+		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite()-(sprite1.longueur()-1) <= sprite2.abscisseLaPlusADroite();
+	}
+
+	private boolean seSuperposeHorinzontalementPartiellementAGauche(Sprite sprite1, Sprite sprite2) {
+		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche()-(sprite1.longueur()-1) && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite();
+	}
+
+	private boolean seSuperposeHorinzontalement(Sprite sprite1, Sprite sprite2) {
+		return sprite1.abscisseLaPlusAGauche() >= sprite2.abscisseLaPlusAGauche() && sprite1.abscisseLaPlusADroite() <= sprite2.abscisseLaPlusADroite();
+	}
+
+	private boolean seSuperposeVerticalement(Sprite sprite1, Sprite sprite2) {
+		return sprite1.ordonneeLaPlusBasse() <= sprite2.ordonneeLaPlusHaute() && sprite1.ordonneeLaPlusHaute() >= sprite2.ordonneeLaPlusBasse();
 	}
 	
 	
